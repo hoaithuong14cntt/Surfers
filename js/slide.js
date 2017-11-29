3873ea362453f832d3557792a8caad129ea7c7c1
Hoai Thuong <thuong.hth@neo-lab.vn>
2017-11-29 15:01:15 +0700

    + add slide in ourteam

--- js/slide.js
+++ js/slide.js
@@ -1,18 +1,3 @@
-// var myIndex = 0;
-// carousel();
-
-// function carousel() {
-//     var i;
-//     var x = document.getElementsByClassName("ourteam-slide-content");
-//     for (i = 0; i < x.length; i++) {
-//        x[i].style.display = "none";  
-//     }
-//     myIndex++;
-//     if (myIndex > x.length) {myIndex = 1}    
-//     x[myIndex-1].style.display = "block";  
-//     setTimeout(carousel, 2000); // Change image every 2 seconds
-// }
-
 var slideIndex = 1;
 showDivs(slideIndex);
 
